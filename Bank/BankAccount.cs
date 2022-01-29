d035430891864dbed813eda352a46b7ebf2fc1a5
d3nnyheat <98631184+d3nnyheat@users.noreply.github.com>
2022-01-29 11:12:40 +0700

    Add files via upload

---
+++ Bank/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        private int banknumber;
+        private decimal balance;
+        private TypeBankAccount type;
+
+        public int BankNumberInfo()
+        {
+            return banknumber;
+        }
+
+        public decimal BalanceInfo()
+        {
+            return balance;
+        }
+        
+        public TypeBankAccount AccountTypeInfo()
+        {
+            return type;
+        }
+
+        public void FillBankNumber(int number)
+        {
+            banknumber = number;
+        }
+
+        public void FillBalance(decimal xbalance)
+        {
+            balance = xbalance;
+        }
+
+        public void FillType(TypeBankAccount typeaccount)
+        {
+            type = typeaccount;
+        }
+
+        public void FullInfo()
+        {
+            Console.WriteLine($"Тип счёта: {type}, Номер счёта: {banknumber}, Баланс: {balance}");
+        }
+    }
+
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            BankAccount B = new BankAccount();
+            B.FillBankNumber(random.Next(1, 999999));
+            B.FillBalance(100000);
+            B.FillType(TypeBankAccount.DEBIT);
+            B.FullInfo();
+        }
+    }
+}
